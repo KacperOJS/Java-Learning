6275f9cd1b3ab13e481e3a7ac5ac656b8f960c18
Kacper <kacperodziemczyk@gmail.com>
2023-07-11 10:24:31 +0200

    first interaction with java

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("What is your name?");
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
+
+		System.out.println("hello " +name);
+
+    }
+	String x = new String();
+}
